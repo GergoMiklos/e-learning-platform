c9a759ebbaf732da0c806378ad9ced40d1d55cb8
Gergo Miklos <GergoM@topdesk.com>
2020-09-17 04:48:53 +0200

    Fixing

--- backend/studyapp/src/main/java/com/thesis/studyapp/dto/TaskInputDto.java
+++ backend/studyapp/src/main/java/com/thesis/studyapp/dto/TaskInputDto.java
@@ -3,11 +3,13 @@ package com.thesis.studyapp.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class TaskInputDto {
     String question;
